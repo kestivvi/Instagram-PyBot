8d782adb603055e7ac94383ce2036f5add4c9aea
Krzysztof Kwiatkowski <kthateyo@gmail.com>
2020-11-27 18:21:18 +0100

    Add error table

--- tools/db.py
+++ tools/db.py
@@ -27,6 +27,13 @@ def check_db():
                             type BYTE NOT NULL
                         )
                     ''')
+        conn.execute('''CREATE TABLE IF NOT EXISTS error
+                        (
+                            ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                            date INT NOT NULL,
+                            message TEXT
+                        )
+                    ''')
     except Error as e:
         print(e)
     finally:
@@ -34,10 +41,13 @@ def check_db():
             conn.close()
 
 
-def insert(type: ACTION):
+def insert(type: ACTION, message: str = ""):
     check_db()
     conn = sqlite3.connect(get_db_path())
-    conn.execute(f'''INSERT INTO user_action (date, type) VALUES (datetime('now'), {type})''');
+    if type == ACTION.ERROR:
+        conn.execute(f'''INSERT INTO error (date, message) VALUES (datetime('now'), "{message}")''');
+    else:
+        conn.execute(f'''INSERT INTO user_action (date, type) VALUES (datetime('now'), {type})''');
     conn.commit()
     conn.close()
 
@@ -45,7 +55,10 @@ def insert(type: ACTION):
 def how_many(type: ACTION, time: int, time_type: str = "Hour") -> int:
     check_db()
     conn = sqlite3.connect(get_db_path())
-    cursor = conn.execute(f'''SELECT count(*) FROM user_action WHERE datetime(date) >= datetime('now', '-{time} {time_type}') AND type = {type}''')
+    if type == ACTION.ERROR:
+        cursor = conn.execute(f'''SELECT count(*) FROM error WHERE datetime(date) >= datetime('now', '-{time} {time_type}')''')
+    else:
+        cursor = conn.execute(f'''SELECT count(*) FROM user_action WHERE datetime(date) >= datetime('now', '-{time} {time_type}') AND type = {type}''')
     result = [row[0] for row in cursor][0]
     conn.close()
     return result
@@ -56,4 +69,8 @@ def db2csv():
     conn = sqlite3.connect(get_db_path(), isolation_level=None,
                            detect_types=sqlite3.PARSE_COLNAMES)
     db_df = pd.read_sql_query("SELECT * FROM user_action", conn)
-    db_df.to_csv('database.csv', index=False)
+    db_df.to_csv(Path(config.data.data_folder) / 'user_action.csv', index=False)
+    db_df = pd.read_sql_query("SELECT * FROM error", conn)
+    db_df.to_csv(Path(config.data.data_folder) / 'error.csv', index=False)
+    conn.close()
+
